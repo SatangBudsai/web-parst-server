eadb6b22654032be24787308cff4894944718c6d
MisterSatang <mastersatang+github@gmail.com>
2023-03-08 10:14:40 +0700

    fix setup tailwind and materialUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@material-tailwind/react/components/Button";
+import Button from "@mui/material/Button";
 
 export default function index() {
   return (
     <>
-      <Button>Button</Button>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   );
 }
