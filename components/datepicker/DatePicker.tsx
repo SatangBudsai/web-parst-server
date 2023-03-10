4b76e180be97388e781ab625676a55d5024ecd45
MisterSatang <mastersatang+github@gmail.com>
2023-03-10 23:11:03 +0700

    setup material and fix datepicker

--- components/datepicker/DatePicker.tsx
+++ components/datepicker/DatePicker.tsx
@@ -4,18 +4,18 @@ import "react-day-picker/dist/style.css";
 
 export default function DatePicker() {
   const [selected, setSelected] = useState<Date>();
-
   return (
     <label>
       <DayPicker
         mode="single"
         showOutsideDays
+        fixedWeeks
         selected={selected}
         onSelect={setSelected}
         captionLayout="dropdown-buttons"
         fromYear={2015}
         toYear={2025}
-        className=" w-fit text-white/80 "
+        className=" w-fit text-white/80"
       />
     </label>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import DatePicker from "@/components/datepicker/DatePicker";
 import Layout from "@/components/layout/Layout";
 import { Icon } from "@iconify/react";
+import Slider from "@mui/material/Slider";
 import { Space_Grotesk } from "next/font/google";
+import { useState } from "react";
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
 export default function index() {
+  const [value, setValue] = useState<number[]>([20, 37]);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+  };
   return (
     <Layout>
       {/* <img
@@ -25,22 +36,22 @@ export default function index() {
           <span>© 2023 MisterSatang. All rights reserved</span>
         </div>
       </div>
-      <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="w-full min-h-screen flex justify-center items-center pt-10">
         <div className="container grid grid-cols-2">
           <div className="col-span-1 flex justify-center">
             <div className="flex flex-col">
-              <h1 className={`${space_Grotesk.className} text-8xl font-bold`}>
+              <h1 className={`${space_Grotesk.className} text-7xl font-bold`}>
                 <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#00AAFF] to-[#00FFAA]">
                   RISING
                 </span>
                 <span className="text-5xl font-bold text-white ml-5">&</span>
               </h1>
-              <h1 className={`${space_Grotesk.className} text-8xl font-bold`}>
+              <h1 className={`${space_Grotesk.className} text-7xl font-bold`}>
                 <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FFAA00] to-[#FF00AA]">
                   SETTING
                 </span>
               </h1>
-              <h2 className="text-white text-xl font-light">
+              <h2 className="text-gray-50 text-xl">
                 Planet approximating of rising and setting time
               </h2>
               <section className="flex flex-row border-2 border-white/10 drop-shadow-xl rounded-gl mt-5 p-3 rounded-2xl">
@@ -58,6 +69,13 @@ export default function index() {
                 </nav>
                 <DatePicker />
               </section>
+              <Slider
+                getAriaLabel={() => "Temperature range"}
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+              />
             </div>
           </div>
         </div>
